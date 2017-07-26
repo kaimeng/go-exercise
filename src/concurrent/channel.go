5fb8adbc85d4685a1ed20d24357b3db3effaac4a
孟凯 <mkk41112@ly.com>
2017-07-26 17:52:51 +0800

    concurrent

---
+++ src/concurrent/channel.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func Count(ch chan int) {
+	ch <- 1
+	fmt.Println("Counting")
+}
+
+func main() {
+	chs := make([]chan int, 10)
+	for i := 0; i < 10; i++ {
+		chs[i] = make(chan int)
+		go Count(chs[i])
+	}
+	for _, ch := range (chs) {
+		<-ch
+	}
+
+	fmt.Println(runtime.NumCPU())
+}
